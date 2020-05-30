b979405acb99118767bb85c450fbebf6761de15a
Daniel Lundin <daniel@arity.se>
2020-05-30 17:36:10 +0200

    figure out a next version

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,15 @@ fi
 BRANCH=$(echo ${BRANCH} | sed -e "s/refs\/heads\///g")
 BRANCH_ALPHA=$(echo ${BRANCH} | sed -e "s/[^a-z0-9]/-/g")
 
-VERSION_CURRENT=$(semver get $(< VERSION))
+VERSION_CURRENT=$(< VERSION)
+VERSION_NEXT=$(semver bump patch ${VERSION_CURRENT})
+
+if [ "${BRANCH}" != "master" ]; then
+  VERSION_NEXT=$(semver bump prerel ${BRANCH_ALPHA} ${VERSION_NEXT})
+fi
+
+echo ${VERSION_NEXT} >VERSION
 
 echo "::set-output name=branch::${BRANCH}"
 echo "::set-output name=branch_alpha::${BRANCH_ALPHA}"
-echo "::set-output name=version_current::${VERSION_CURRENT}"
+echo "::set-output name=version::${VERSION_NEXT}"
