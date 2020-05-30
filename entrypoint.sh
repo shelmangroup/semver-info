16a823a20cc9c7724d9405a77c745bd7754bbdc5
Daniel Lundin <daniel@arity.se>
2020-05-30 17:06:25 +0200

    Read VERSION file

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,8 @@ fi
 BRANCH=$(echo ${BRANCH} | sed -e "s/refs\/heads\///g")
 BRANCH_ALPHA=$(echo ${BRANCH} | sed -e "s/[^a-z0-9]/-/g")
 
+VERSION_CURRENT=$(semver $(< VERSION))
+
 echo "::set-output name=branch::${BRANCH}"
 echo "::set-output name=branch_alpha::${BRANCH_ALPHA}"
+echo "::set-output name=version_current::${VERSION_CURRENT}"
