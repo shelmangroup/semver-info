1d7d9a7862c9ee5a7639530b7f77438a6db466cd
Daniel Lundin <dln@arity.se>
2020-05-30 11:08:29 +0200

    Testing versioning

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,8 @@ fi
 BRANCH=$(echo ${BRANCH} | sed -e "s/refs\/heads\///g")
 BRANCH_ALPHA=$(echo ${BRANCH} | sed -e "s/[^a-z0-9]/-/g")
 
+date
+env | sort
+
 echo "::set-output name=branch::${BRANCH}"
 echo "::set-output name=branch_alpha::${BRANCH_ALPHA}"
