5a3be0c18af39694921d7042c6c54105baa545cb
Daniel Lundin <daniel@arity.se>
2020-05-30 17:43:38 +0200

    fiddle with meta

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,10 +12,10 @@ VERSION_CURRENT=$(< VERSION)
 VERSION_NEXT=$(semver bump patch ${VERSION_CURRENT})
 
 if [ "${BRANCH}" != "master" ]; then
-  VERSION_NEXT=$(semver bump prerel dev ${VERSION_NEXT})
+  VERSION_NEXT=$(semver bump prerel ${BRANCH_ALPHA} ${VERSION_NEXT})
 fi
 
-VERSION_NEXT=${VERSION_NEXT}+${BRANCH_ALPHA}.${GITHUB_RUN_NUMBER}
+VERSION_NEXT=${VERSION_NEXT}+${GITHUB_RUN_NUMBER}
 
 echo ${VERSION_NEXT} >VERSION
 echo "::set-output name=branch::${BRANCH}"
