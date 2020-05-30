7fef6a069bd9e957a61c357cc17b688190bd21f3
Daniel Lundin <daniel@arity.se>
2020-05-30 10:58:56 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ fi
 BRANCH=$(echo ${BRANCH} | sed -e "s/refs\/heads\///g")
 BRANCH_ALPHA=$(echo ${BRANCH} | sed -e "s/[^a-z0-9]/-/g")
 
+date
 env | sort
 
 echo "::set-output name=branch::${BRANCH}"
