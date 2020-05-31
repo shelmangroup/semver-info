8f2b8337b7eac84f36f2c59b6fe51c110cf6fa01
Daniel Lundin <daniel@arity.se>
2020-05-31 07:08:24 +0200

    add sha

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,11 +13,11 @@ VERSION_NEXT=$(semver bump patch ${VERSION_CURRENT})
 
 if [ "${BRANCH}" != "master" ]; then
   VERSION_NEXT=$(semver bump prerel ${BRANCH_ALPHA}.${GITHUB_RUN_NUMBER} ${VERSION_NEXT})
+  VERSION_NEXT=${VERSION_NEXT}+${GITHUB_SHA:0:8}
 fi
 
-VERSION_NEXT=${VERSION_NEXT}+${GITHUB_SHA:0:8}
-
 echo ${VERSION_NEXT} >VERSION
+
 echo "::set-output name=branch::${BRANCH}"
 echo "::set-output name=branch_alpha::${BRANCH_ALPHA}"
 echo "::set-output name=version::${VERSION_NEXT}"
